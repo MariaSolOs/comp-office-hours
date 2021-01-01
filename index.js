758ee4a0904965b2a03b7263372d034f390eb598
Maria Solano <majosolano99@gmail.com>
2021-01-01 15:12:43 -0500

    Fix Heroku issue

--- index.js
+++ index.js
@@ -21,9 +21,11 @@ const { ApolloServer } = require('apollo-server-express'),
        mongoClient = require('./config/mongoDB'),
       { Instructors, Students, Appointments } = require('./datasources'),
        express = require('express'),
+       path = require('path'),
        cors = require('cors');
 
 const app = express();
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 app.use(cors({
     origin: process.env.CLIENT_URL,
     credentials: true
@@ -44,6 +46,10 @@ const server = new ApolloServer({
     })
 });
 server.applyMiddleware({ app });
+
+app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
 app.listen({ port: process.env.PORT || 4000 }, () => {
     console.log(`Server ready at ${server.graphqlPath}`);
